064b133d12cfb7e8ace54770d908df6d0f8106eb
Kyle R <kyle.rinehart83@gmail.com>
2019-06-18 18:43:48 -0400

    Learning string methods

--- Hello World.py
+++ Hello World.py
@@ -9,3 +9,9 @@ last_name = "Rinehart"
 age = 36
 
 print("My name is "+first_name+" "+last_name+".  I am "+str(age)+" years old.")
+
+occupation = "Junior Mid Senior Web Developer"
+
+current_occupation = occupation[:6] + " " + occupation[18:31]
+
+print("I am a " + current_occupation + ".")
